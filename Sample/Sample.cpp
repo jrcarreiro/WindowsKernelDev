f8a4359f438c109a22b285443f7208301e2f4c08
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-09 09:42:52 -0300

    Include KdPrint

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -3,6 +3,8 @@
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
     UNREFERENCED_PARAMETER(DriverObject);
+
+    KdPrint(("Sample driver Unload called\n"));
 }
 
 extern "C"
@@ -13,6 +15,9 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
     UNREFERENCED_PARAMETER(RegistryPath);
 
     DriverObject->DriverUnload = SampleUnload;
+
+    KdPrint(("Sample driver initialized successfully\n"));
+
 
     return STATUS_SUCCESS;
 }
