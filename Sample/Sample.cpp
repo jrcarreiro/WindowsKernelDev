dfc442c1f4cff8cb3c4235a018de27809dd35605
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-10 09:55:59 -0300

    RtlGetVersion function to retrieve the information

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -7,17 +7,25 @@ void SampleUnload(_In_ PDRIVER_OBJECT DriverObject)
     KdPrint(("Sample driver Unload called\n"));
 }
 
+
+
 extern "C"
 NTSTATUS
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) 
 {
     UNREFERENCED_PARAMETER(DriverObject);
     UNREFERENCED_PARAMETER(RegistryPath);
-
+    
     DriverObject->DriverUnload = SampleUnload;
 
     KdPrint(("Sample driver initialized successfully\n"));
-
+    
+    RTL_OSVERSIONINFOEXW osverinfo = { sizeof(osverinfo) };
+    RtlGetVersion((PRTL_OSVERSIONINFOW)&osverinfo);
+    KdPrint(("OSVersion NT %d.%d:%d sp%d.%d\n",
+	   osverinfo.dwMajorVersion, osverinfo.dwMinorVersion, osverinfo.dwBuildNumber,
+	   osverinfo.wServicePackMajor, osverinfo.wServicePackMinor));
 
     return STATUS_SUCCESS;
 }
+
