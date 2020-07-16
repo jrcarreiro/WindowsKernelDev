90ed5468bf0ba25c6b9c165d3bbf424858625d15
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-16 08:22:15 -0300

    Create header file

--- PriorityBooster/PriorityBooster.cpp
+++ PriorityBooster/PriorityBooster.cpp
@@ -1,8 +1,12 @@
 #include <ntddk.h>
+#include "PriorityBoosterCommon.h"
 
 // prototypes
 
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject);
+
+NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+
 
 // DriverEntry
 
@@ -10,6 +14,10 @@ extern "C" NTSTATUS
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
     DriverObject->DriverUnload = PriorityBoosterUnload;
+
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
+    DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = PriorityBooterDeviceControl;
 
     return STATUS_SUCCESS;
 }
