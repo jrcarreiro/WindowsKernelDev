bdcc5fb0837ac4e33e668159103a5cf5aa0c4fb1
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-17 10:00:29 -0300

    Fixed

--- PriorityBooster/PriorityBooster.cpp
+++ PriorityBooster/PriorityBooster.cpp
@@ -6,7 +6,6 @@
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject);
 
 NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
-NTSTATUS IoCreateDevice()
 
 // DriverEntry
 
