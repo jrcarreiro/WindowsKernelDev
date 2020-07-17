da59b3d255cb35af3817648dad680170f2422131
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-17 10:12:30 -0300

    Create a client code

---
+++ Booster/Booster.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <stdio.h>
+#include "..\PriorityBooster\PriorityBoosterCommon.h"
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 3)
+    {
+	   printf("Usage: Booster <threadid> <priority>\n");
+	   return 0;
+    }
+}
