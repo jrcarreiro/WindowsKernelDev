3ca0b7dfb6d8ce547abb8f85f8772b3148048f65
Junior Carreiro <junior.carreiro@outlook.com>
2020-07-17 09:58:30 -0300

    Delete Device

--- PriorityBooster/PriorityBooster.cpp
+++ PriorityBooster/PriorityBooster.cpp
@@ -30,10 +30,24 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	   return status;
     }
 
+    UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+    status = IoCreateSymbolicLink(&symLink, &devName);
+    if (!NT_SUCCESS(status))
+    {
+	   KdPrint(("Failed to create symbolic link (x%08X)\n", status));
+	   IoDeleteDevice(DeviceObject);
+	   return status;
+    }
+
     return STATUS_SUCCESS;
 }
 
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
+    UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+    // delete symbolik link
+    IoDeleteSymbolicLink(&symLink);
 
+    // delete device object
+    IoDeleteDevice(DriverObject->DeviceObject);
 }
